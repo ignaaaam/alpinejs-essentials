928d5dde7a2674fc83cfa7cf49250704acb44e1d
iamatnuria <ignasiamat10@gmail.com>
2022-03-28 15:24:01 +0200

    Data binding done

---
+++ resources/views/data-binding.blade.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/alpinejs/2.3.0/alpine.js" integrity="sha512-nIwdJlD5/vHj23CbO2iHCXtsqzdTTx3e3uAmpTm4x2Y8xCIFyWu4cSIV8GaGe2UNVq86/1h9EgUZy7tn243qdA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script src="https://cdn.tailwindcss.com"></script>
+    <title>DATA BINDING</title>
+</head>
+<body>
+    <div x-data="{first: 0, second: 0}">
+
+        <input class="border" type="text" x-model.number="first"> + <input class="border " type="text" x-model.number="second"> = <output x-text="first + second" ></output>
+
+    </div>
+</body>
+</html>
